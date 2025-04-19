14bd4ee9e3b87187680e8428bf94f8611cbee151
Thato Keith Kujwane <developer.tk_kujwane@outlook.com>
2025-04-19 05:33:59 +0200

    Paused working on sys tray override code

--- 19 April WMI Implementation/system_tray.py
+++ 19 April WMI Implementation/system_tray.py
@@ -1,31 +1,14 @@
 from pystray import Icon, Menu, MenuItem
 from PIL import Image
 import sys
-from PyQt5.QtWidgets import QApplication, QInputDialog
 
 class SystemTrayApp:
-    def __init__(self, icon_name, image_path, override_code="mecio"):
-        # Initialize the override code and attempts log
+    def __init__(self, icon_name, image_path):
         self.icon_name = icon_name
         self.image_path = image_path
         self.icon = None
-        self.OVERRIDE_CODE = override_code
 
     def quit_application(self, icon, item):
-        def prompt_override_code():
-            app = QApplication(sys.argv)
-            code, ok = QInputDialog.getText(None, "Override Code", "Enter the override code:")
-            if ok:
-                return code
-            return None
-
-        entered_code = prompt_override_code()
-        if entered_code == self.OVERRIDE_CODE:
-            print("Override code accepted. Quitting application.")
-        else:
-            print("Invalid override code. Application will not quit.")
-            return
-        # Quit the application
         self.icon.stop()
 
     def create_image(self):
